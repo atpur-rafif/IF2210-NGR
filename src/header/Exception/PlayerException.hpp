539d123cf0a932673c842748ce4b9f3bc497c26c
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-11 00:26:09 +0700

    feat: player exception

---
+++ src/header/Exception/PlayerException.hpp
@@ -0,0 +1,19 @@
+#ifndef EXCEPTION_PLAYEREXCEPTION_HPP
+#define EXCEPTION_PLAYEREXCEPTION_HPP
+
+#include "Exception/GameException.hpp"
+#include <string>
+#include <map>
+using namespace std;
+
+class PlayerException : public GameException {};
+
+class NotEnoughResourceException : public PlayerException {
+private:
+    string message;
+public:
+    NotEnoughResourceException(map<string,int> &remainingResource, int &remainingMoney);
+	const char *what() const noexcept;
+};
+
+#endif
