83bbc066991060d89d9e1ab8849ec95162123306
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-11 21:31:13 +0700

    fix:  missing itemFactoryexception

---
+++ src/header/Exception/ItemFactoryException.hpp
@@ -0,0 +1,14 @@
+#ifndef EXCEPTION_ITEMFACTORYEXCEPTION_HPP
+#define EXCEPTION_ITEMFACTORYEXCEPTION_HPP
+
+#include "Exception/GameException.hpp"
+using namespace std;
+
+class ItemFactoryException : public GameException {};
+
+class ItemNotFoundException : public ItemFactoryException {
+public:
+	const char *what() const noexcept override;
+};
+
+#endif
