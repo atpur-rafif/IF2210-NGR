70dfda5357b5f747a1e40480aded3766c17e2af9
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-04-12 23:54:46 +0700

    feat: printInventory

--- src/code/View/CLI/Shop.cpp
+++ src/code/View/CLI/Shop.cpp
@@ -4,34 +4,69 @@
 #include <iostream>
 using namespace std;
 
-template <class T> void Shop::sellItem(Player &player) {
-  ProductItem *product;
-  cout << "Berikut merupakan penyimpanan Anda\n     ================[ "
-          "Penyimpanan ]==================\n";
-  PlayerView::printInventory(player);
-  cout << "Silahkan pilih petak yang ingin Anda jual!\nPetak : ";
-  string location;
-  cin >> location;
-  if (location == "CANCEL")
-    throw UserCancelledPlayerViewException();
-  try {
-    auto rawItem = player.inventory.getItem(location);
-    if (!rawItem.has_value()) {
-      cout << "Tidak ada item pada lokasi ini!\n";
+template <class T>
+void Shop::playerSellItem(Player &player) {
+    cout << "Berikut merupakan penyimpanan Anda\n     ================[ Penyimpanan ]==================\n";
+    PlayerView::printInventory(player);
+    cout << "Silahkan pilih petak yang ingin Anda jual!\nPetak : ";
+    string location;
+    cin >> location;
+    if (location == "CANCEL")
+        throw UserCancelledPlayerViewException();
+    try {
+        auto rawItem = player.inventory.getItem(location);
+        if (!rawItem.has_value()) {
+            cout << "Tidak ada item pada lokasi ini!\n";
+        }
+
+        T *casted = dynamic_cast<T *>(rawItem->getRaw());
+        if (casted == NULL) {
+            cout << "Invalid item type\n";
+        };
+
+        ProductItem *product = casted;
+        if (shopInventory[product->getCode()] != -1)
+            shopInventory[product->getCode()]++;
+        
+        player.weight -= product->getAddedWeight();
+        player.money += product->getPrice();
+        player.inventory.clearItem(location);
+    } catch (const exception &err) {
+        cout << err.what() << endl;
     }
-
-    T *casted = dynamic_cast<T *>(rawItem->getRaw());
-    if (casted == NULL) {
-      cout << "Invalid item type\n";
-    };
-
-    *product = *casted;
-    player.weight -= product->getAddedWeight();
-    player.money += product->getPrice();
-    player.inventory.clearItem(location);
-  } catch (const exception &err) {
-    cout << err.what() << endl;
-  }
 }
 
-template <class T> void Shop::buyItem(Player &player) {}
+template <class T>
+void Shop::playerBuyItem(Player &player) {
+    vector<Heapify<Item>> vec;
+    player.inventory.getAllItem(&vec)
+    this->printInventory();
+    pair<int, int> size = player.inventory.getSize();
+    cout << "\nUang Anda : " << player.money <<
+    "\nSlot penyimpanan tersedia : " << (size.first * size.second) - vec.size() <<
+    "\nBarang ingin dibeli : ";
+    int select, qty;
+    cin >> select;
+    cout << "\nKuantitas : ";
+    cin >> qty;
+
+    // CONTINUE
+
+
+
+}
+
+void Shop::printInventory(map<string, int> &m) {
+    cout << "Selamat datang di toko!!\nBerikut merupakan hal yang dapat Anda Beli\n";
+    int i = 0;
+    for(const auto& [key, value] : m) {
+        if (value != 0) {
+            cout << i << ". " << key << " - " << "price" << " (" << "quantity" << ")\n";
+            ++i;
+        }
+        else if(value == -1) {
+            cout << i << ". " << key << " - " << "price\n";
+            ++i;
+        }
+    }
+}
--- src/header/View/CLI/Shop.hpp
+++ src/header/View/CLI/Shop.hpp
@@ -8,55 +8,27 @@
 using namespace std;
 
 class Shop : public PlayerView {
-
 public:
-  map<string, int> shopInventory{// Plant
-                                 {"TEK", -1},
-                                 {"SDT", -1},
-                                 {"ALT", -1},
-                                 {"IRN", -1},
-                                 {"APL", -1},
-                                 {"ORG", -1},
-                                 {"BNT", -1},
-                                 {"GAV", -1},
+    map<string, int> shopInventory{// Plant
+                                   {"TEK", -1}, {"SDT", -1}, {"ALT", -1}, {"IRN", -1}, {"APL", -1}, {"ORG", -1}, {"BNT", -1}, {"GAV", -1},
 
-                                 // Animal
-                                 {"COW", -1},
-                                 {"SHP", -1},
-                                 {"HRS", -1},
-                                 {"RBT", -1},
-                                 {"SNK", -1},
-                                 {"CHK", -1},
-                                 {"DCK", -1},
+                                   // Animal
+                                   {"COW", -1}, {"SHP", -1}, {"HRS", -1}, {"RBT", -1}, {"SNK", -1}, {"CHK", -1}, {"DCK", -1},
 
-                                 // Product
-                                 {"TAW", 0},
-                                 {"SAW", 0},
-                                 {"ALW", 0},
-                                 {"IRW", 0},
-                                 {"APP", 0},
-                                 {"ORP", 0},
-                                 {"BNP", 0},
-                                 {"GAP", 0},
-                                 {"COM", 0},
-                                 {"SHM", 0},
-                                 {"HRM", 0},
-                                 {"RBM", 0},
-                                 {"SNM", 0},
-                                 {"CHM", 0},
-                                 {"DCM", 0},
-                                 {"CHE", 0},
-                                 {"DCE", 0},
+                                   // Product
+                                   {"TAW", 0}, {"SAW", 0}, {"ALW", 0}, {"IRW", 0}, {"APP", 0}, {"ORP", 0}, {"BNP", 0}, {"GAP", 0}, {"COM", 0}, {"SHM", 0}, {"HRM", 0}, {"RBM", 0}, {"SNM", 0}, {"CHM", 0}, {"DCM", 0}, {"CHE", 0}, {"DCE", 0},
 
-                                 // Building Recipe
-                                 {"SMH", 0},
-                                 {"MDH", 0},
-                                 {"LRH", 0},
-                                 {"HTL", 0}};
+                                   // Building Recipe
+                                   {"SMH", 0}, {"MDH", 0}, {"LRH", 0}, {"HTL", 0}
+								   };
 
-  template <class T> void sellItem(Player &player);
+    template <class T>
+    void playerSellItem(Player &player);
 
-  template <class T> void buyItem(Player &player);
+	template <class T>
+	void playerBuyItem(Player &player);
+
+    void printInventory(map<string, int> &m);
 };
 
 #endif
