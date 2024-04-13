b7f93937ac67672e9feb489bddcbe9ef18218fe7
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-13 19:49:07 +0700

    Username already exist exception

--- src/code/Exception/PlayerControllerException.cpp
+++ src/code/Exception/PlayerControllerException.cpp
@@ -3,3 +3,7 @@
 const char *InvalidPlayerTypeException::what() const noexcept {
 	return "Invalid Player Type";
 }
+
+const char *UsernameAlreadyExist::what() const noexcept {
+	return "Username already exist";
+}
