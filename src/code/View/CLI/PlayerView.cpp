127e15fdc6c3f84523e195726eb35ab15fb5ef03
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-04-16 10:49:31 +0700

    fix: EXIT not working while on a different prompt

--- src/code/View/CLI/PlayerView.cpp
+++ src/code/View/CLI/PlayerView.cpp
@@ -13,100 +13,108 @@ using namespace std;
 PlayerView::~PlayerView(){};
 
 void PlayerView::start(Player &player) {
-	while (true) {
-		player.getContext().getPlayerController().checkWinner();
-		function<string(string)> fn = [](string input) {
-			return input;
-		};
-		string prompt = player.getUsername() + ">";
-		string command = CLI::prompt(prompt, fn);
+  while (true) {
+    player.getContext().getPlayerController().checkWinner();
+    function<string(string)> fn = [](string input) { return input; };
+    string prompt = player.getUsername() + ">";
+    string command = CLI::prompt(prompt, fn);
 
-		if (command == "NEXT") {
-			auto list_player = player.getContext().getPlayerController().getPlayers();
-			int size = list_player.size();
-			for (int i = 0; i < size; ++i) {
-				auto farmer_player = list_player.at(i).get();
-				if (farmer_player->getType() == FarmerType) {
-					Farmer &farmer = *(dynamic_cast<Farmer *>(farmer_player));
-					farmer.plantsGrow();
-				}
-			}
-			return;
-		}
+    if (command == "NEXT") {
+      auto list_player = player.getContext().getPlayerController().getPlayers();
+      int size = list_player.size();
+      for (int i = 0; i < size; ++i) {
+        auto farmer_player = list_player.at(i).get();
+        if (farmer_player->getType() == FarmerType) {
+          Farmer &farmer = *(dynamic_cast<Farmer *>(farmer_player));
+          farmer.plantsGrow();
+        }
+      }
+      return;
+    }
 
-		try {
-			try {
-				this->runPlayerCommand(player, command);
-				continue;
-			} catch (const CommandNotFoundCLIException &_) {
-			} catch (...) {
-				throw;
-			}
-			this->runSpecializedPlayerCommand(player, command);
-			continue;
-		} catch (const CLIException &err) {
-			cout << err.what() << endl;
-		}
-	}
+    try {
+      try {
+        this->runPlayerCommand(player, command);
+        continue;
+      } catch (const CommandNotFoundCLIException &_) {
+      } catch (...) {
+        throw;
+      }
+      this->runSpecializedPlayerCommand(player, command);
+      continue;
+    } catch (const UserCancelledCLIException &err) {
+      cout << err.what() << endl;
+    }
+  }
 }
 
 void PlayerView::eat(Player &player) {
-	auto items = player.inventory.getAllItem();
-	bool found = false;
-	for (auto item : items) {
-		if ((*item)->getType() != Product) continue;
+  auto items = player.inventory.getAllItem();
+  bool found = false;
+  for (auto item : items) {
+    if ((*item)->getType() != Product)
+      continue;
 
-		shared_ptr<ProductItem> product = dynamic_pointer_cast<ProductItem>(*item);
-		if (product->getProductItemType() != MaterialProduct) {
-			found = true;
-			break;
-		}
-	}
+    shared_ptr<ProductItem> product = dynamic_pointer_cast<ProductItem>(*item);
+    if (product->getProductItemType() != MaterialProduct) {
+      found = true;
+      break;
+    }
+  }
 
-	if (!found) {
-		cout << "Kamu tidak memiliki barang yang bisa dimakan!" << endl;
-		return;
-	}
+  if (!found) {
+    cout << "Kamu tidak memiliki barang yang bisa dimakan!" << endl;
+    return;
+  }
 
-	PlayerView::printInventory(player);
-	function<void(string, optional<shared_ptr<Item>> &)> fn = [](string, optional<shared_ptr<Item>> &opt) {
-		if (!opt.has_value())
-			throw PromptException("Tidak bisa memakan petak kosong");
+  PlayerView::printInventory(player);
+  function<void(string, optional<shared_ptr<Item>> &)> fn =
+      [](string, optional<shared_ptr<Item>> &opt) {
+        if (!opt.has_value())
+          throw PromptException("Tidak bisa memakan petak kosong");
 
-		shared_ptr<ProductItem> product = dynamic_pointer_cast<ProductItem>(opt.value());
-		if (product == nullptr)
-			throw PromptException("Barang yang dipilih tidak bisa dimakan");
+        shared_ptr<ProductItem> product =
+            dynamic_pointer_cast<ProductItem>(opt.value());
+        if (product == nullptr)
+          throw PromptException("Barang yang dipilih tidak bisa dimakan");
 
-		if (product->getProductItemType() == MaterialProduct)
-			throw PromptException("Tidak bisa memakan material");
-	};
-	string location = CLI::promptStorageLocation("Petak untuk dimakan: ", player.inventory, fn);
-	player.eat(location);
+        if (product->getProductItemType() == MaterialProduct)
+          throw PromptException("Tidak bisa memakan material");
+      };
+  string location =
+      CLI::promptStorageLocation("Petak untuk dimakan: ", player.inventory, fn);
+  player.eat(location);
 }
 
 void PlayerView::save(Player &player) {
-	cout << "Location to save: ";
-	string location;
-	cin >> location;
-	Config::writeState(location, player.getContext());
+  cout << "Location to save: ";
+  string location;
+  cin >> location;
+  Config::writeState(location, player.getContext());
 }
 
 void PlayerView::runPlayerCommand(Player &player, string command) {
-	if (command == "BELI") ShopView::buyItem(player);
-	else if (command == "JUAL") ShopView::sellItem(player);
-	else if (command == "CETAK_PENYIMPANAN") this->printInventory(player);
-	else if (command == "MAKAN") this->eat(player);
-	else if (command == "STATUS") {
-		cout << "Money: " << player.getMoney() << endl;
-		cout << "Weight: " << player.getWeight() << endl;
-	} else if (command == "SIMPAN") this->save(player);
-	else throw CommandNotFoundCLIException();
+  if (command == "BELI")
+    ShopView::buyItem(player);
+  else if (command == "JUAL")
+    ShopView::sellItem(player);
+  else if (command == "CETAK_PENYIMPANAN")
+    this->printInventory(player);
+  else if (command == "MAKAN")
+    this->eat(player);
+  else if (command == "STATUS") {
+    cout << "Money: " << player.getMoney() << endl;
+    cout << "Weight: " << player.getWeight() << endl;
+  } else if (command == "SIMPAN")
+    this->save(player);
+  else
+    throw CommandNotFoundCLIException();
 }
 
 void PlayerView::printInventory(Player &player) {
-	function<string(shared_ptr<Item> &)> fn = [](shared_ptr<Item> &item) {
-		return item->getCode();
-	};
+  function<string(shared_ptr<Item> &)> fn = [](shared_ptr<Item> &item) {
+    return item->getCode();
+  };
 
-	CLI::printStorage("Penyimpanan", player.inventory, fn);
+  CLI::printStorage("Penyimpanan", player.inventory, fn);
 };
